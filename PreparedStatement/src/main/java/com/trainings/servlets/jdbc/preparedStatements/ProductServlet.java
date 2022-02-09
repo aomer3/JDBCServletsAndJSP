715e181179728993d302471dd301b57b7db8b74e
aomer3 <67030297+aomer3@users.noreply.github.com>
2022-02-09 10:08:50 -0500

    Added study notes on prepared statements

---
+++ PreparedStatement/src/main/java/com/trainings/servlets/jdbc/preparedStatements/ProductServlet.java
@@ -0,0 +1,75 @@
+package com.trainings.servlets.jdbc.preparedStatements;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ProductServlet
+ */
+@WebServlet("/ProductServlet")
+public class ProductServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Connection con;
+	private PreparedStatement ps;
+		public void init() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost/mydb","root","GoReadMe2021!");
+			
+			//PreparedStatements use "?" placeholders that will later be set to values
+			ps = con.prepareStatement("insert into product values(?,?,?,?);"); 
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Integer id = Integer.parseInt(request.getParameter("id"));
+		String name = request.getParameter("name");
+		String description = request.getParameter("description");
+		Integer price = Integer.parseInt(request.getParameter("price"));
+	
+		try {
+			ps.setInt(1, id);
+			ps.setString(2, name);
+			ps.setString(3, description);
+			ps.setInt(4, price);
+			
+			int result = ps.executeUpdate();
+			
+			response.setContentType("text/html");
+			PrintWriter out = response.getWriter();
+			
+			out.print("<b>" + result + " Products Created </b>");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void destroy() {
+		try {
+			con.close();
+			ps.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
